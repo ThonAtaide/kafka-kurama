453ce690f48c2329cff59c4e2e475717ccdf6478
ThonAtaide <thon_ataide10@hotmail.com>
2022-04-07 22:24:26 -0300

    initial commit

---
+++ src/main/java/br/com/ataide/kafkakurama/KafkaKuramaApplication.java
@@ -0,0 +1,13 @@
+package br.com.ataide.kafkakurama;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaKuramaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaKuramaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/ataide/kafkakurama/KafkaKuramaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ataide.kafkakurama;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaKuramaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
