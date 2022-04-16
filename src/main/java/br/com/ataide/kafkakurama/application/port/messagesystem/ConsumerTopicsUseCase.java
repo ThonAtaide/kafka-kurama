1c7733570e20877467256e977fa683938eaba197
ThonAtaide <thon_ataide10@hotmail.com>
2022-04-16 18:58:01 -0300

    added open api descriptions into controller

--- src/main/java/br/com/ataide/kafkakurama/application/port/messagesystem/ConsumerTopicsUseCase.java
+++ src/main/java/br/com/ataide/kafkakurama/application/port/messagesystem/ConsumerTopicsUseCase.java
@@ -3,7 +3,6 @@ package br.com.ataide.kafkakurama.application.port.messagesystem;
 import br.com.ataide.kafkakurama.application.port.dto.TopicInfoDto;
 import lombok.NonNull;
 
-import java.util.Collection;
 import java.util.List;
 import java.util.Properties;
 
--- src/main/java/br/com/ataide/kafkakurama/userinterface/rest/interfaces/KafkaConsumerController.java
+++ src/main/java/br/com/ataide/kafkakurama/userinterface/rest/interfaces/KafkaConsumerController.java
@@ -1,18 +1,23 @@
 package br.com.ataide.kafkakurama.userinterface.rest.interfaces;
 
 import br.com.ataide.kafkakurama.application.port.vo.TopicInfoVo;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
 
-@RequestMapping("/consumer")
+@RequestMapping("/topic")
+@Tag(name="Topic resource operations")
 public interface KafkaConsumerController {
 
-    @GetMapping("/topic")
+    @GetMapping
+    @Operation(description = "Return all kafka topics.")
     List<String> getTopics();
 
-    @GetMapping("/topic/{topicName}")
+    @GetMapping("/{topicName}")
+    @Operation(description = "Return information about the topic informed.")
     List<TopicInfoVo> getTopicInfo(@PathVariable(name = "topicName") String topicName);
 }
